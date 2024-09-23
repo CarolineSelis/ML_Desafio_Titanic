7f6bccbedaa30f73f373d14e6ca48ba8212a86d7
Caroline Selis <38839189+CarolineSelis@users.noreply.github.com>
2024-09-23 08:52:02 -0300

    Add files via upload

---
+++ ML_TITANIC.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(patchwork)
+library(dplyr)
+library(caret)
+library(e1071)
+
+dataset <- read.csv('/kaggle/input/pearsons-height-data-simple-linear-regression/Pearson.csv')
